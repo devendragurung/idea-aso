57554926119d26d1a6a50820120408edb4c51d2e
devendragurung <i23004.devendra@iica.jp>
2024-11-29 15:22:20 +0900

    Update db.php

--- graduation/db.php
+++ graduation/db.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection using PDO
-$host = 'mysql311.phy.lolipop.lan';          // or your DB host
-$dbname = 'LAA1516492-keziban';    // your database name
-$username = 'LAA1516492';           // your DB username
-$password = '1234';               // your DB password
+$host = '';          // or your DB host
+$dbname = '';    // your database name
+$username = '';           // your DB username
+$password = '';               // your DB password
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
